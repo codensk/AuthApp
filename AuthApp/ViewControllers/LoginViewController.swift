66499c00988a0f261036bcac99355394fd7efc6e
Sergey Vorobev <rusbear28@yandex.ru>
2021-05-31 10:13:37 +0700

    Extension refactoring

--- AuthApp/ViewControllers/LoginViewController.swift
+++ AuthApp/ViewControllers/LoginViewController.swift
@@ -115,3 +115,25 @@ class LoginViewController: UIViewController {
         present(alert, animated: true)
     }
 }
+
+// MARK: - Extension
+extension LoginViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        switch textField {
+        case usernameInput:
+            passwordInput.becomeFirstResponder()
+        case passwordInput:
+            performSegue(withIdentifier: "welcomeVCSegue", sender: self)
+        default:
+            break
+        }
+
+        return true
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        
+        view.endEditing(true)
+    }
+}
